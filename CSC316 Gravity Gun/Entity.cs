0ffa49f2abba2e6248942285e186dd767c5b3512
Brett Napier <brettnapier@yahoo.com>
2017-03-24 18:16:00 -0400

    Add Entity class
    Add a class to store an Entity (object in 3d space).  Add get/set methods for Entity name, position, velocity, weight and gravity.

---
+++ CSC316 Gravity Gun/Entity.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC316_Gravity_Gun
+{
+    class Entity
+    {
+        /// <summary>
+        /// The name of the entity
+        /// </summary>
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        /// <summary>
+        /// The position of the entity
+        /// </summary>
+        Vector3 positon;
+        public Vector3 Position
+        {
+            get { return positon; }
+            set { positon = value; }
+        }
+
+        /// <summary>
+        /// The velocity of the entity
+        /// </summary>
+        Vector3 velocity;
+        public Vector3 Velocity
+        {
+            get { return velocity; }
+            set { velocity = value; }
+        }
+
+        /// <summary>
+        /// The weight of the entity
+        /// </summary>
+        float weight;
+        public float Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+        /// <summary>
+        /// The strength of gravity applied to the entity
+        /// </summary>
+        float gravity; 
+        public float Gravity
+        {
+            get { return gravity; }
+            set { gravity = value; }
+        }
+    }
+}
