bc31e7b1811b434b5ddfe0b082916ac49276a3f5
Brett Napier <brettnapier@yahoo.com>
2017-03-31 18:58:55 -0400

    Add dealDamage() and takeDamage() methods to Creature Class
    Add the dealDamage() method that calls the takeDamage() method of another Creature so that the creatures health is reduced by the damageDealable of the attacking creature.

--- CSC316 Gravity Gun/Creature.cs
+++ CSC316 Gravity Gun/Creature.cs
@@ -19,22 +19,29 @@ namespace CSC316_Gravity_Gun
         int speed;
 
         /// <summary>
+        /// The amount of damage that a creature can do to another creature
+        /// </summary>
+        int damageDealable;
+
+        /// <summary>
         /// Default constructor for a creature
         /// </summary>
         public Creature() : base()
         {
             health = 1000;
             speed = 1;
+            damageDealable = 10;
         }
         /// <summary>
         /// Constructor to set health and speed of a Creature
         /// </summary>
         /// <param name="h">Health of the Creature</param>
         /// <param name="s">Speed the Creature moves at</param>
-        public Creature(int h, int s) : base()
+        public Creature(int h, int s, int d) : base()
         {
             health = h;
             speed = s;
+            damageDealable = d;
         }
         /// <summary>
         /// 
@@ -44,10 +51,28 @@ namespace CSC316_Gravity_Gun
         /// <param name="v">Velocity to give the Creature</param>
         /// <param name="h">Health of the Creature</param>
         /// <param name="s">Speed the Creature moves at</param>
-        public Creature(string n, Vector3 p, Vector3 v, int h, int s) : base(n, p, v)
+        public Creature(string n, Vector3 p, Vector3 v, int h, int s, int d) : base(n, p, v)
         {
             health = h;
             speed = s;
+            damageDealable = d;
         }
+        /// <summary>
+        /// Deals damage to another creature
+        /// </summary>
+        /// <param name="creature"></param>
+        public void dealDamage(Creature creature)
+        {
+            creature.takeDamage(damageDealable);
+        }
+        /// <summary>
+        /// Takes damage from another creature
+        /// </summary>
+        /// <param name="amount">Amount of damage to take</param>
+        public void takeDamage(int amount)
+        {
+            health -= amount; //reduce health by damage amount
+        }
+
     }
 }
